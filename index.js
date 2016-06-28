c7cd6346c61f5e8768974c554db86d38b80be954
Tian Tian3 Zhang <zhangtian3@Lenovo.com>
2016-06-28 17:11:08 +0800

    library v1.0.0

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var sqlite=require('sqlite3');
 var app = express();
+var db=new sqlite.Database('library.db');
 
 app.use(express.static('public'));
 
@@ -7,6 +9,149 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname+"/public/html/login.html");
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+// 登陆判断用户名和密码
+app.get('/login', function(req, res){
+  var userName=req.query.userName;
+  var password=req.query.password;
+  db.all('select * from user where user=?;',[userName],function(err,ress){
+  if(ress.length ==1){
+	  if(ress[0].password==password){
+		  res.end('1'+ress[0].auth);	
+		} else {
+        	  res.end('password wrong');
+      		}
+      } else {
+      res.end('no user');
+    }
+  });			
+});
+
+// 注册判断用户名和邮箱
+app.get('/register',function(req,res){
+	var userName=req.query.userName;
+  	var password=req.query.password;
+  	var email=req.query.email;
+  	var sqluser='select * from user where user=?'+[userName];
+  	var sqlemail='select * from user where email=?'+[email];
+  	db.all('select * from user where user=?;',[userName],function(err,ress){
+  		if(ress.length !=0){
+  			res.end('userName is used');
+  		} else {
+  			db.all('select * from user where email=?;',[email],function(err,ress){
+  				if(ress.length !=0){
+  					res.end('email is used');
+  				} else {
+  					// 用户名和邮箱不存在，可以写入数据库
+            db.run('insert into user (user,password,email,auth) values(?,?,?,?);',[userName,password,email,"user"],function(err){
+              if(err){
+                res.end('something wrong,retry');
+              } else {
+                res.end('1');
+              }
+            }); 
+  				}
+  			});
+  		}
+  	});
+});
+
+// 展示在前端的可选图书
+app.get('/library',function(req,res){
+  db.all('select * from books where use=?',['false'],function(err,data){
+    res.send(data);
+  });
+});
+
+// 展示在前端的当前用户可借图书
+app.get('/return',function(req,res){
+  var userName=req.query.user;
+  db.all('select * from needReturn where user=? and isOK=? group by book',[userName,'no'],function(err,ress){
+    res.send(ress);
+  });
+});
+
+// 用户申请结束后写入log，并将图书改为不可见
+app.get('/borrow',function(req,res){
+  var userName=req.query.user;
+  var books=req.query.books;
+  var isOK=[];
+  for(var i=0;i<books.length;i++){
+    db.run('insert into log (user,book,time,how,isOK) values(?,?,?,?,?)',[userName,books[i],Date(),'borrow','no'],function(err,data){
+      if(err){
+        res.end('something wrong,retry');
+      } else {
+        isOK.push(true);
+      }
+    });
+    db.run('update books set use=? where book=?',['true',books[i]],function(errs,datas){
+          
+    });
+  }
+  if(!isOK.sort()[0]){
+    res.end('1');
+  }
+});
+
+// 用户申请还书后写入log
+app.get('/back',function(req,res){
+  var userName=req.query.user;
+  var books=req.query.books;
+  var isOK=[];
+  for(var i=0;i<books.length;i++){
+    db.run('insert into log (user,book,time,how,isOK) values(?,?,?,?,?)',[userName,books[i],Date(),'back','no'],function(err,data){
+      if(err){
+        res.end('something wrong,retry');
+      } else {
+        isOK.push(true);
+      }
+    });
+  }
+  if(!isOK.sort()[0]){
+    res.end('1');
+  }
+});
+
+// 申请查看，管理员
+app.get('/apply',function(req,res){
+  db.all('select user,book,time,how,isOK from log where isOK=? group by book',['no'],function(err,data){
+    res.send(data);
+  });
+});
+
+// 管理员处理
+app.get('/answerApply',function(req,res){
+  var userName=req.query.userName;
+  var book=req.query.book;
+  var how=req.query.how;
+  var agree=req.query.agree;
+  if(how == 'borrow'){
+    if(agree == 'true'){
+      db.run('update log set isOK=? where user=? and book=? and how=?',['yes',userName,book,how]);
+      db.run('insert into needReturn (user,book,isOK) values (?,?,?)',[userName,book,'no']);
+      db.run('update books set available=? , use=? where book=?',[0,'false',book],function(err,ress){
+        res.end('1');
+      });
+    } else {
+      db.run('update log set isOK=? where user=? and book=? and how=?',['yes',userName,book,how]);
+      db.run('update books set use=? where book=?',['false',book],function(err,ress){
+        res.end('1');
+      });
+    }
+  } else {
+    if(agree == 'true'){
+      db.run('update log set isOK=? where user=? and book=? and how=?',['yes',userName,book,how]);
+      db.run('update needReturn set isOK=? where user=? and book=?',['yes',userName,book]);
+      db.run('update books set available=? , use=? where book=?',[1,'false',book],function(err,ress){
+        res.end('1');
+      });
+    } else {
+      db.run('update log set isOK=? where user=? and book=? and how=?',['yes',userName,book,how],function(err,ress){
+        res.end('1');
+      });
+    }
+  }
+});
+
+app.listen(3000,function(){
+  console.log('library starting');
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,289 @@
 $(document).ready(function(){
+			// 登陆按钮，用户名密码判断	
+			
+			var thisUser;
+			var thisAuth;
+
 			$('#login').click(function(){
 				var userName=$('[name=userName]').val();
 				var password=$('[name=password]').val();
 				var data={};
 				data.userName=userName;
 				data.password=password;
-				alert(JSON.stringify(data));
+
+					$.ajax({
+						url:'/login',
+						data:data,
+						success:function(data){
+							if(data[0]=='1'){
+								thisUser=userName;
+								thisAuth=data.slice(1);
+								alert('欢迎来到图书馆 '+thisUser);
+								$('#main').hide();
+								$('#library').show();
+								libraryInit();
+								if(thisAuth == 'admin'){
+									$('#admin').show();
+									adminInit();
+								}
+							} else {
+								alert(data);
+							}
+						}
+					});				
 			});
 
+			// 注册按钮，显示注册页面
 			$('#register').click(function(){
 				$('#main').hide();
 				$('#enroll').show();
 			});
 
+			// 注册确认按钮，确保输入的信息有效，并存入数据库
+			// 有效性条件：1.不为空，2.两个密码一致，3.后台无此用户名,4.lenovo邮箱
 			$('#sure').click(function(){
 				var userName=$('[name=userNameEnroll]').val();
 				var password=$('[name=passwordEnroll]').val();
 				var rePassword=$('[name=rePasswordEnroll]').val();
-				if(userName.length ==0 || password.length ==0){
-					alert('不能为空');
-				} else if(password != rePassword){
-					alert('密码不一致');
+				var email=$('[name=email]').val();
+				var emailReg=/.+@lenovo.com$/;
+
+				if(userName.length>0){
+					if(emailReg.test(email)){
+						if(userName.length ==0 || password.length ==0){
+							alert('不能为空');
+						} else if(password != rePassword){
+							alert('密码不一致');
+						} else {
+							// 都填对了
+							var data={};
+							data.userName=userName;
+							data.password=password;
+							data.email=email;
+							$.ajax({
+								url:'/register',
+								data:data,
+								success:function(data){
+									if(data=='1'){
+										$('#enroll').hide();
+										$('#main').show();
+									} else {
+										alert(data);
+									}
+								}
+							});	
+						}
+					} else {
+						alert("请输入lenovo邮箱");
+					}
 				} else {
-					$('#main').show();
-					$('#enroll').hide();
+					alert("请输入用户名");
 				}
+			});
+
+			// 图书馆初始化
+			function libraryInit(){
+				$.ajax({
+					url:'/library',
+					success:function(data){
+						for(var i=0;i<data.length;i++){
+							if(data[i].available == 1){
+								var str='<div class="label"><span><input type="checkbox" class="select"></span><span>'+data[i].book+'</span></div>';
+								$('#available').append(str);
+							}
+						}
+					}
+				});
+				$.ajax({
+					url:'/return',
+					data:{'user':thisUser},
+					success:function(data){
+						for(var i=0;i<data.length;i++){
+							var str='<div class="label"><span><input type="checkbox" class="reSelect"></span><span>'+data[i].book+'</span></div>';
+								$('#inavailable').append(str);
+						}
+					}
+				});
+			}
+
+			// 管理员初始化
+			function adminInit(){
+				$.ajax({
+					url:'/apply',
+					success:function(data){
+						for(var i=0;i<data.length;i++){
+							if(data[i].how=='borrow'){
+								// 借书申请
+								var userName=data[i].user;
+								var book=data[i].book;
+								var time=new Date(data[i].time);
+								var showtime=(time.getMonth()+1)+'月'+time.getDate()+'日';
+								var str='<div class="label"><span>'+userName+'</span><span>'+book+'</span><span>'+showtime+'</span><span><button class="agree">同意</button><button class="disagree">不同意</button></span></div>';
+								$('#applyBorrow').append(str);
+							} else {
+								//还书申请
+								var userName=data[i].user;
+								var book=data[i].book;
+								var time=new Date(data[i].time);
+								var showtime=(time.getMonth()+1)+'月'+time.getDate()+'日';
+								var str='<div class="label"><span>'+userName+'</span><span>'+book+'</span><span>'+showtime+'</span><span><button class="agree">同意</button><button class="disagree">不同意</button></span></div>';
+								$('#applyReturn').append(str);
+							}
+						}
+						// 提交申请
+						applySubmit();
+					}
+				});
+			}
+
+			function applySubmit(){
+				var $applyBorrowAgree=$('#applyBorrow .agree');
+				var $applyBorrowDisagree=$('#applyBorrow .disagree');
+				var $applyReturnAgree=$('#applyReturn .agree');
+				var $applyReturnDisagree=$('#applyReturn .disagree');
+				$applyBorrowAgree.each(function(){
+					$(this).click(function(){
+						var $this=$(this);
+						var data={};
+						data.userName=$(this).parent().prev().prev().prev().text();
+						data.book=$(this).parent().prev().prev().text();
+						data.how='borrow';
+						data.agree='true';
+						// 可以post
+						$.ajax({
+							url:'answerApply',
+							data:data,
+							success:function(err,datas){
+								if(datas=='success'){
+									$this.parent().parent().hide();
+								} else {
+									alert('something wrong, retry');
+								}
+							}
+						});
+					});
+				});
+				$applyBorrowDisagree.each(function(){
+					$(this).click(function(){
+						var $this=$(this);
+						var data={};
+						data.userName=$(this).parent().prev().prev().prev().text();
+						data.book=$(this).parent().prev().prev().text();
+						data.how='borrow';
+						data.agree='false';
+						// 可以post
+						$.ajax({
+							url:'answerApply',
+							data:data,
+							success:function(err,datas){
+								if(datas=='success'){
+									$this.parent().parent().hide();
+								} else {
+									alert('something wrong, retry');
+								}
+							}
+						});
+					});
+				});
+				$applyReturnAgree.each(function(){
+					$(this).click(function(){
+						var $this=$(this);
+						var data={};
+						data.userName=$(this).parent().prev().prev().prev().text();
+						data.book=$(this).parent().prev().prev().text();
+						data.how='back';
+						data.agree='true';
+						// 可以post
+						$.ajax({
+							url:'answerApply',
+							data:data,
+							success:function(err,datas){
+								if(datas=='success'){
+									$this.parent().parent().hide();
+								} else {
+									alert('something wrong, retry');
+								}
+							}
+						});
+					});
+				});
+				$applyReturnDisagree.each(function(){
+					$(this).click(function(){
+						var $this=$(this);
+						var data={};
+						data.userName=$(this).parent().prev().prev().prev().text();
+						data.book=$(this).parent().prev().prev().text();
+						data.how='back';
+						data.agree='false';
+						// 可以post
+						$.ajax({
+							url:'answerApply',
+							data:data,
+							success:function(err,datas){
+								if(datas=='success'){
+									$this.parent().parent().hide();
+								} else {
+									alert('something wrong, retry');
+								}
+							}
+						});
+					});
+				});
+			}
+
+			// 确认借书
+			$('#borrow').click(function(){
+				var selected=$('.select:checked');
+				var books=[];
+				if(selected.length>0){
+					selected.each(function(){
+						var book=$(this).parent().next().text();
+						books.push(book);
+					});
+				}
+				alert('确认要借：'+books);
+				var postData={'user':thisUser,'books':books};
+				$.ajax({
+					url:'/borrow',
+					data:postData,
+					success:function(data){
+						if(data[0]=='1'){
+							alert('借书成功，请前往9A024取书');
+							selected.each(function(){
+								$(this).parent().parent().hide();
+							});
+						} else {
+							alert('借书失败，请稍后重试');
+						}
+					}
+				});
+			});
+
+			// 确认还书
+			$('#resend').click(function(){
+				var selected=$('.reSelect:checked');
+				var books=[];
+				if(selected.length>0){
+					selected.each(function(){
+						var book=$(this).parent().next().text();
+						books.push(book);
+					});
+				}
+				alert('确认要还：'+books);
+				var postData={'user':thisUser,'books':books};
+				$.ajax({
+					url:'/back',
+					data:postData,
+					success:function(data){
+						if(data[0]=='1'){
+							alert('还书成功，请送完9A024');
+							selected.each(function(){
+								$(this).parent().parent().hide();
+							});
+						} else {
+							alert('还书失败，请稍后重试');
+						}
+					}
+				});
 			});
 	});
